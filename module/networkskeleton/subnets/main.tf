9b34d89a34eb4cdec0a0ef31ba02065c467b8488
Ishandev01 <129931982+Ishandev01@users.noreply.github.com>
2023-04-12 13:37:15 +0530

    Update main.tf

--- module/networkskeleton/subnets/main.tf
+++ module/networkskeleton/subnets/main.tf
@@ -1,17 +1,12 @@
 resource "google_compute_subnetwork" "private" {
-<<<<<<< HEAD
      count = length(var.cidr_range)
      name = var.pri_subnet_name[count.index]
      ip_cidr_range = var.cidr_range[count.index]
-=======
-    name = "private"
-     ip_cidr_range = var.cidr_range
->>>>>>> a2a74c93de091713a5c74986a3a76853eb32eea8
      region = var.region
      network = var.vpcnetwork
      private_ip_google_access = true
 }
-<<<<<<< HEAD
+
 resource "google_compute_subnetwork" "public" {
     network = var.vpcnetwork
     count = length(var.pub_subnet_cidr_block)
@@ -19,5 +14,3 @@ resource "google_compute_subnetwork" "public" {
     name = var.pub_subnet_name[count.index]
     region = var.regionpub
 }
-=======
->>>>>>> a2a74c93de091713a5c74986a3a76853eb32eea8
