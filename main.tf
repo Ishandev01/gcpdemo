a7c08e8fe8ef334b2a768f4e20f09125bccbebf5
Ishandev01 <129931982+Ishandev01@users.noreply.github.com>
2023-04-12 12:11:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ source = "../module/networkskeleton/vpc"
 vpc_name = var.vpc_name
 }
 module "subnet" {
-source = "../module/networkskeletonsubnet"
+source = "../module/networkskeleton/subnet"
 cidr_range = var.cidr_range
 region = var.region
 }
