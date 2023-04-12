a602ee63298c5ebf5a17f80838dce4fef6eb91f0
Ishandev01 <129931982+Ishandev01@users.noreply.github.com>
2023-04-12 12:31:28 +0530

    Update main.tf

--- module/networkskeleton/subnets/main.tf
+++ module/networkskeleton/subnets/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_subnetwork" "private" {
     name = "private"
-     ip_cidr_range = "var.cidr_range"
-     region = "var.region"
-     network = "var.vpcnetwork"
+     ip_cidr_range = var.cidr_range
+     region = var.region
+     network = var.vpcnetwork
      private_ip_google_access = true
 }
