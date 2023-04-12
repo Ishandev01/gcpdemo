a9bcc51c58e1373804b2fcbb9c42b2b3a2db57ff
Ishandev01 <129931982+Ishandev01@users.noreply.github.com>
2023-04-12 10:22:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+resource "google_project_service" "service_account" {
+  for_each = toset([    
+    "compute.googleapis.com",
+    "servicenetworking.googleapis.com",
+  ])
+  service = each.key
+
+  project            = "githubdemo-383406"
+  disable_on_destroy = false
+  disable_dependent_services = true
+}
+
+
 resource "google_compute_network" "vpc" {
 name = "test-demo"
 auto_create_subnetworks = false
