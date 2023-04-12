3120c64014b1fa6256dbd490d377b23b16ed2997
Ishandev01 <129931982+Ishandev01@users.noreply.github.com>
2023-04-12 12:29:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ vpc_name = var.vpc_name
 }
 module "subnet" {
 source = "./module/networkskeleton/subnets"
+vpcnetwork = google_compute_network.vpc.vpc_id
 cidr_range = var.cidr_range
 region = var.region
 }
