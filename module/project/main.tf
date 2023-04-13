56fdfae98ea132b66c51522e01cd327a42cdd894
Ishan <ishanhoney1@gmail.com>
2023-04-13 08:58:29 +0000

    updated code 15

---
+++ module/project/main.tf
@@ -0,0 +1,15 @@
+resource "google_service_account" "service_account" {
+  account_id   = var.gcp_service_account_name
+  display_name = var.gcp_service_account_name
+  project      = "githubdemo-383406"
+}
+
+resource "google_project_service" "service_account" {
+  for_each = toset([    
+    "container.googleapis.com",
+    "servicenetworking.googleapis.com",
+  ])
+  service = each.key
+
+  project = "githubdemo-383406"
+}
