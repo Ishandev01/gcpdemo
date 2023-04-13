ec4fb93dc8af5bbd991758104ca90578f4742d27
Ishan <ishanhoney1@gmail.com>
2023-04-13 09:32:34 +0000

    updated code 16

--- module/gke/main.tf
+++ module/gke/main.tf
@@ -49,10 +49,10 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
     tags = ["mydemo-cluster"]
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.cicdgit.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    # # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    # service_account = google_service_account.cicdgit.email
+    # oauth_scopes    = [
+    #   "https://www.googleapis.com/auth/cloud-platform"
+    # ]
   }
 }
