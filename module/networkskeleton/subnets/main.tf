9430c649908952e9639fb0aeecfb348a418617e2
Ishan <ishanhoney1@gmail.com>
2023-04-14 10:50:14 +0000

    updated code 21

--- module/networkskeleton/subnets/main.tf
+++ module/networkskeleton/subnets/main.tf
@@ -7,7 +7,7 @@ resource "google_compute_subnetwork" "private" {
      private_ip_google_access = true
 
     secondary_ip_range = { 
-        count = length(var.podrange)
+        count = length(var.podrangename)
        ip_cidr_range = var.podrange[count.index]
        range_name = var.podrangename [count.index]
      }
